8eeb8301e219a990e0a5f7576a984774fe7ac31a
samuelstefancic <samuelstefancic@gmail.com>
2023-02-23 20:48:50 +0100

    First step of the project

---
+++ server/src/main/java/work/sam/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package work.sam.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
